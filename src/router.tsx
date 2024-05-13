64990cb7df42452edaaaedf8c6c8885dd1c36c23
Luiz Felipe <felipe.pinghang2@gmail.com>
2024-05-13 20:06:52 -0300

    feat: adicionando arquivos iniciais

---
+++ src/router.tsx
@@ -0,0 +1,14 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/home";
+import NotFoundPage from "./pages/_notFound";
+
+export default function Router() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="*" element={<NotFoundPage />} />
+            </Routes>
+        </BrowserRouter>
+    )
+}
